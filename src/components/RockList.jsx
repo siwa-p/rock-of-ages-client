832ff241af99d2d5f0acd7400eda4505dcf8ff77
Steve Brownlee <chortlehoort@gmail.com>
2023-10-02 21:51:40 -0500

    Showing rock owners

--- src/components/RockList.jsx
+++ src/components/RockList.jsx
@@ -8,8 +8,8 @@ export const RockList = ({ rocks, fetchRocks }) => {
     const displayRocks = () => {
         if (rocks && rocks.length) {
             return rocks.map(rock => <div key={`key-${rock.id}`} className="border p-5 border-solid hover:bg-fuchsia-500 hover:text-violet-50 rounded-md border-violet-900 mt-5 bg-slate-50">
-                <div>{rock.name} ({rock.type.label}) weighs {rock.weight} kg</div>
-                <div>In the collection of {rock.user.first_name} { rock.user.last_name } </div>
+                <div>{rock.name} ({rock.type.label})</div>
+                <div>In the collection of {rock.user.first_name} {rock.user.last_name}</div>
             </div>)
         }
 
